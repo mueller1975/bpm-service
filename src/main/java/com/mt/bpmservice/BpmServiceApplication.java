fdc1da9aa67e2a983638a3834a3e99f3f9879740
mueller_tsai <mueller_tsai@walsin.com>
2023-07-25 17:14:16 +0800

    7.25 created from office

---
+++ src/main/java/com/mt/bpmservice/BpmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mt.bpmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/bpmservice/BpmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.bpmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
